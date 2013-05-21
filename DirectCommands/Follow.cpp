c4b64fa7971ee09d346f86118fe8a85e701d745e
OpenTLDbot <opentldbot@mhrtw.(none)>
2013-05-21 19:57:22 +0300

    alages sti Follow

--- DirectCommands/Follow.cpp
+++ DirectCommands/Follow.cpp
@@ -17,21 +17,26 @@ int Follow::run(int centerOfBB[2])
 	//if we have a target
         if(centerOfBB[0] != -1)
         {
-                //target in the middle
-                if(WIDTH/3 <= centerOfBB[0] && centerOfBB[0] <= 2*WIDTH/3 && range > STOPDISTANCE)
-                {                move.forward(250);
+		if(range > STOPDISTANCE)
+		{
+                	//target in the middle
+                	if(WIDTH/3 <= centerOfBB[0] && centerOfBB[0] <= 2*WIDTH/3)
+                	{                
+				move.forward(250);
                 		printf("forward\n");
-		}//target is not in the middle
-                if(centerOfBB[0] < WIDTH/3 || centerOfBB[0] > 2*WIDTH/3)
-                {
-                        float rotVelW = (2*centerOfBB[0] - WIDTH) / WIDTH; //mapping ths theshs X se times [-1 1] analoga me th thesh sto frame
-                        move.turn(-rotVelW*maxRotVel);
-			printf("turn r=%f, 2*x=%d,-w=%d \n",rotVelW,2*centerOfBB[0],WIDTH);
-                }
-                if(range < STOPDISTANCE)
-                        return 3;
-
-                return 1;
+			}//target is not in the middle
+                	if(centerOfBB[0] < WIDTH/3 || centerOfBB[0] > 2*WIDTH/3)
+                	{
+                        	float rotVelW = (2*centerOfBB[0] - WIDTH) / WIDTH; //mapping ths theshs X se times [-1 1] analoga me th thesh sto frame
+                        	move.turn(-rotVelW*maxRotVel);
+				printf("turn r=%f, 2*x=%d,-w=%d \n",rotVelW,2*centerOfBB[0],WIDTH);
+                	}
+			//Flow to Follow
+			return 1;
+		}
+		//TODO edw prepei na doume ti 8a kanoume me tn periptosi pou to empodio den einai o stoxos mas
+		else
+			move.stop();
 
         }
         //We have no target
